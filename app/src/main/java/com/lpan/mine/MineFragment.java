e41762360d2c4a5da0d9a89ba75b57b5db13e281
LiuPan <1573802540@qq.com>
2018-03-09 22:38:23 +0800

    更变了APP 图标等

--- app/src/main/java/com/lpan/mine/MineFragment.java
+++ app/src/main/java/com/lpan/mine/MineFragment.java
@@ -10,72 +10,48 @@ import com.lpan.messagepush.MessagePushActivity;
 import com.lpan.mine.databinding.DataBindingDemoActivity;
 import com.lpan.qrcodescanner.qrcode.QrCodeActivity;
 
-public class MineFragment extends Fragment {
+public class MineFragment extends Fragment implements View.OnClickListener {
     View view;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         if(view==null){
             view = inflater.inflate(R.layout.mine_fragment, container, false);
             View mine_message_push = view.findViewById(R.id.mine_message_push);
             View  mine_qr_scanner =  view.findViewById(R.id.mine_qr_scanner);
             View  browser =  view.findViewById(R.id.mine_browser);
             View  mine_data_binding =  view.findViewById(R.id.mine_data_binding);
+            View  mine_data_encrypted_currency =  view.findViewById(R.id.mine_data_currency);
 
-            mine_message_push.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    startActivity(new Intent(getContext(),MessagePushActivity.class));
-                }
-            });
-            mine_qr_scanner.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    startActivity(new Intent(getContext(),QrCodeActivity.class));
-                }
-            });
-            browser.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    startActivity(new Intent(getContext(),BrowserActivity.class));
-                }
-            });
-            mine_data_binding.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    startActivity(new Intent(getContext(),DataBindingDemoActivity.class));
-                }
-            });
-
-
-
-/*            Bitmap src = BitmapFactory.decodeResource(getResources(), R.mipmap.react); //获取Bitmap图片
-            RoundedBitmapDrawable roundedBitmapDrawable = RoundedBitmapDrawableFactory.create(getResources(), src); //创建RoundedBitmapDrawable对象
-            float size = getResources().getDimension(R.dimen.mine_head_size) / 2;
-            LogUtil.i("========onCreateView===========size:"+size);
-            roundedBitmapDrawable.setCornerRadius(size+50); //设置圆角半径（根据实际需求）
-          //  roundedBitmapDrawable.setAntiAlias(true); //设置反走样
-            mine_img_head.setImageDrawable(roundedBitmapDrawable); //显示圆角图片*/
-
-
-
-      /*      Bitmap src = BitmapFactory.decodeResource(getResources(),  R.mipmap.icon_drawer_portrait);
-            Bitmap dst;
-//将长方形图片裁剪成正方形图片
-            if (src.getWidth() >= src.getHeight()){
-                dst = Bitmap.createBitmap(src, src.getWidth()/2 - src.getHeight()/2, 0, src.getHeight()-10, src.getHeight()-10);
-            }else{
-                dst = Bitmap.createBitmap(src, 0, src.getHeight()/2 - src.getWidth()/2, src.getWidth(), src.getWidth()
-                );
-            }
-            RoundedBitmapDrawable roundedBitmapDrawable = RoundedBitmapDrawableFactory.create(getResources(), dst);
-            roundedBitmapDrawable.setCornerRadius(dst.getWidth() / 2); //设置圆角半径为正方形边长的一半
-            roundedBitmapDrawable.setAntiAlias(true);
-            mine_img_head.setImageDrawable(roundedBitmapDrawable);*/
+            mine_message_push.setOnClickListener(this);
+            mine_qr_scanner.setOnClickListener(this);
+            browser.setOnClickListener(this);
+            mine_data_binding.setOnClickListener(this);
+            mine_data_encrypted_currency.setOnClickListener(this);
         }
-
         return view;
     }
 
 
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.mine_message_push:
+                startActivity(new Intent(getContext(),MessagePushActivity.class));
+                break;
+            case R.id.mine_qr_scanner:
+                startActivity(new Intent(getContext(),QrCodeActivity.class));
+                break;
+            case R.id.mine_browser:
+                startActivity(new Intent(getContext(),BrowserActivity.class));
+                break;
+            case R.id.mine_data_binding:
+                startActivity(new Intent(getContext(),DataBindingDemoActivity.class));
+                break;
+            case R.id.mine_data_currency:
+              //  startActivity(new Intent(getContext(),DataCurrencyMonitorActivity.class));
+                break;
+        }
+    }
 }
